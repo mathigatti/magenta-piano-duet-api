c13fbf9da3990e6fc50d7468c02d344362e79fc4
Mathi Gatti <mathigatti@gmail.com>
2020-10-08 14:25:08 -0300

    qpm

--- predict.py
+++ predict.py
@@ -38,13 +38,13 @@ def _steps_to_seconds(steps, qpm):
     return steps * 60.0 / qpm / steps_per_quarter
 
 
-def make_midi(pitches, start_times, durations, tempo, midi_path):
+def make_midi(pitches, start_times, durations, qpm, midi_path):
     track    = 0
     channel  = 0
     time     = 0
     volume   = 100
     MyMIDI = MIDIFile(1)
-    MyMIDI.addTempo(track, time, tempo)
+    MyMIDI.addTempo(track, time, qpm)
 
     for pitch, start_time, duration in zip(pitches,start_times,durations):
         MyMIDI.addNote(track, channel, pitch, start_time, duration, volume)
@@ -52,20 +52,20 @@ def make_midi(pitches, start_times, durations, tempo, midi_path):
     with open(midi_path, "wb") as output_file:
         MyMIDI.writeFile(output_file)
 
-def make_notes_sequence(pitches, start_times, durations, tempo):
+def make_notes_sequence(pitches, start_times, durations, qpm):
     TEMP_MIDI = "temp.mid"
-    make_midi(pitches, start_times, durations, tempo, TEMP_MIDI)
+    make_midi(pitches, start_times, durations, qpm, TEMP_MIDI)
     return mm.midi_file_to_sequence_proto(TEMP_MIDI)
 
 def generate_midi(pitches, start_times, durations, tempo, length):
-    primer_sequence = make_notes_sequence(pitches, start_times, durations, tempo)
+    qpm = tempo/2
+    primer_sequence = make_notes_sequence(pitches, start_times, durations, qpm)
 
     generator_options = generator_pb2.GeneratorOptions()
     # Set the start time to begin on the next step after the last note ends.
     last_end_time = (max(n.end_time for n in primer_sequence.notes)
                      if primer_sequence.notes else 0)
 
-    qpm = tempo/2 # I'm not sure about this
     generator_options.generate_sections.add(
         start_time=last_end_time + _steps_to_seconds(1, qpm),
         end_time=length)
