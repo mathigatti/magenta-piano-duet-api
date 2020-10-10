5bf6db34449143facb0d6e89b5a692065c77ac04
Mathias Gatti <mathigatti@gmail.com>
2020-10-09 21:20:56 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import requests
 import json
 
-endpoint = "https://ai-duet-ilfqxfroaq-uc.a.run.app"
+production_endpoint = "https://ai-duet-ilfqxfroaq-uc.a.run.app"
+local_endpoint = "http://localhost:8080"
+endpoint = production_endpoint
 
 def compute_next_notes(pitches, durs, start_times, tempo, length=4):
     headers = {
@@ -17,42 +19,11 @@ def compute_next_notes(pitches, durs, start_times, tempo, length=4):
     response = requests.post(f'{endpoint}/predict', headers=headers, data=json.dumps(midi_data))
     return response.json()
 
+pitches = [60,60,61,62]
+start_times = [0,1,2,3]
+durations = [1,1,1,2]
+tempo = 120
+length = 10
 
-def midi2note(note):
-    midi2note_dict = {int(Scale.default.get_midi_note(pitch)): pitch for pitch in range(-100,100)} # I'm going to hell for this
-    if note in midi2note_dict:
-        return midi2note_dict[note]
-    else:
-        return midi2note_dict[note-1] + 0.5
-
-def foxdot_values(pitches, durs, length=4):
-    print(pitches)
-    pitches = [int(Scale.default.get_midi_note(pitch)) for pitch in list(pitches)]
-    print(pitches)
-    durs = list(durs)
-    start_times = [sum(durs[:i]) for i in range(len(durs))]
-    data = compute_next_notes(pitches, durs, start_times, int(Clock.bpm), length)        
-    pitches = [midi2note(int(pitch)) for pitch in data["pitches"]]
-    durs = [start_2 - start_1 for start_1, start_2 in zip(data["start_times"][:-1],data["start_times"][1:])]
-    sus = data["durations"]
-    print(pitches,durs,sus)
-    return pitches, durs, sus
-
-# Initial values
-pitches = PWalk()[:15]
-durs = PSum(15,4)
-sus = durs
-
-# Or maybe starting empty
-pitches = []
-durs = []
-sus = []
-
-# New values
-Clock.bpm = 120
-
-pitches, dur, sus = foxdot_values(pitches[-8:],durs[-8:], length=64)
-
-d1 >> piano(pitches,dur=dur,sus=sus)
-
-
+next_notes = compute_next_notes(pitches, durs, start_times, tempo, length)
+print(next_notes)
